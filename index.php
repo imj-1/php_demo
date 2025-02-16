f029adfe87325343d7b062d53a9cafbcba0dcd12
imj-1 <isidromolina.322@gmail.com>
2025-02-16 15:38:11 -0500

    Functions and Filters

--- index.php
+++ index.php
@@ -14,22 +14,45 @@
             [
                 'name' => "Do Android Dreams of Electric Sheep",
                 'author' => "Philip K. Dick",
+                'releaseYear' => 1968,
                 'purchaseUrl' => 'https://example.com'
             ],
             [
                 'name' => "Project Hail Mary",
                 'author' => "Andy Weir",
+                'releaseYear' => 2021,
                 'purchaseUrl' => 'https://example.com'
-            ]
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'https://example.com'
+            ],
         ];
+
+        function filterByAuthor($books, $author) {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book; // append book to filteredBooks
+                }
+            }
+
+            return $filteredBooks;
+        }
+
     ?>
+
+
 
     <ul>
         <!--  shorthand syntax for loops -->
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterByAuthor($books, 'Andy Weir') as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
                 </a>
             </li>
         <?php endforeach; ?>
